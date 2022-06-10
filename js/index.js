53d19a916383547d711257cd6434a17bee8628ac
estebanzarate <esteban-zarate@hotmail.com>
2022-06-09 21:17:19 -0300

    gallery img

--- js/index.js
+++ js/index.js
@@ -1,15 +1,21 @@
 document.addEventListener('click', e => {
 	if (e.target.matches('.gallery-img')) {
-		const galleryImgModal = document.querySelector('.gallery-img-modal');
+		const galleryImgModal = document.querySelector('.gallery-modal');
 		galleryImgModal.innerHTML += `
-			<div class='img-modal-container'>
-				<img src='${ String(e.target.src).replace('https://estebanzarate.github.io/DWProject-Coder', '..') }'  alt='${e.target.alt}'>
+			<div class='gallery-modal-container'>
+				<img class='gallery-modal-img' src='${ String(e.target.src).replace('https://estebanzarate.github.io/DWProject-Coder', '..')}'  alt='${e.target.alt}'>
 			</div>
 		`;
 		galleryImgModal.classList.add('img-modal-visible');
+		const galleryModalContainer = document.querySelector('.gallery-modal-container');
+		const galleryModalImg = document.querySelector('.gallery-modal-img');
+		if (galleryModalImg.clientHeight > galleryModalContainer.clientHeight) {
+			galleryModalImg.style.height = '100%';
+			galleryModalImg.style.width = 'unset';
+		}
 	}
-	if (e.target.matches('.gallery-img-modal')) {
-		document.querySelector('.img-modal-container').remove();
-		document.querySelector('.gallery-img-modal').classList.remove('img-modal-visible');
+	if (e.target.matches(['.gallery-modal-container', '.gallery-modal'])) {
+		document.querySelector('.gallery-modal-container').remove();
+		document.querySelector('.gallery-modal').classList.remove('img-modal-visible');
 	}
 });
